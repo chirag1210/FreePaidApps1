18565acbe846d336e24b2d6d8d64ac539b477a2f
chirag1210 <chirag.chaudhari7@gmail.com>
2017-12-12 13:09:15 +0530

    sample1

---
+++ app/src/paid/java/itg/com/iconcolorchanger/Constats.java
@@ -0,0 +1,14 @@
+package itg.com.iconcolorchanger;
+
+/**
+ * Created by Chirag Chaudhari on 12,December,2017
+ */
+public class Constats {
+
+    public enum Type{
+        FREE,PAID
+    }
+
+
+    public static  final  Type type= Type.PAID;
+}
